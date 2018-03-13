d033bb0f0a13ced12d7e9d2623e194f0ca067d50
Brittany Morris <brittmmendez@gmail.com>
2018-03-13 19:28:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function distanceTravelledInFeet(startBlock, endBlock){
   return Math.abs(startBlock-endBlock)*264
 }
 
-
 function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination);
   if (distance < 400) {
